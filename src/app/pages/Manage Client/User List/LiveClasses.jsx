b4efed4633c09e5d8435d94fd20c86a1a9affe5e
Akshaypareek01 <akshay96102@gmail.com>
2023-12-22 18:20:30 +0530

    working

--- src/app/pages/Manage Client/User List/LiveClasses.jsx
+++ src/app/pages/Manage Client/User List/LiveClasses.jsx
@@ -17,7 +17,7 @@ import { useNavigate } from 'react-router-dom'
 import EnergySavingsLeafIcon from '@mui/icons-material/EnergySavingsLeaf';
 import { UserWalletModal } from './Components/UserWalletModal'
 import AddVehicle from './Components/AddVehicle'
-import { BASE_URL } from '../../../Config/BaseUrl'
+import { BASE_URL, Base_url } from '../../../Config/BaseUrl'
 import axios from 'axios'
 import DeleteIcon from '@mui/icons-material/Delete';
 import BorderColorIcon from '@mui/icons-material/BorderColor';
@@ -28,29 +28,189 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import events from "./Events";
 import HostMeeting from './HostMeeting'
-// import timeGridPlugin from "@fullcalendar/timegrid";
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import { createTheme } from "@mui/material/styles";
+import { ThemeProvider } from '@mui/material/styles';
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const orangeTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#fff', // Set the main color to your desired shade of orange
+    },
+  },
+});
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+    },
+  },
+};
+function CustomTabPanel(props) {
+  const { children, value, index, ...other } = props;
 
-// import "@fullcalendar/core/main.css";
-// import "@fullcalendar/daygrid/main.css";
-// import "@fullcalendar/timegrid/main.css";
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ paddingTop:2 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
 
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
 
-
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+const column=[
+  {name:"Title"},
+  {name:"Teacher"},
+  {name:"Schedule Date"},
+  {name:"Recordings"},
+  {name:"View"},
+  {name:"Meeting"}
+  
+]
 const LiveClasses = () => {
   const navigate = useNavigate();
   const[ShowVideo,setShowVideo] = useState(false)
+  const [value, setValue] = React.useState(0);
+  const [rows, setRows] = useState([]);
+  const [classes, setClasses] = useState([]);
+  const [Events, setEvents] = useState([]);
+  const [filterRows,setFilterRows] = useState([])
+  const [update,setupdate] = useState(0)
   const [ZoomMeetingNumber,setZoomMeetingNumber] = useState({
     number:"",
     pass:""
   });
-  const handelZoomMeeting = ()=>{
+  const handelZoomMeeting = (Data)=>{
+    
+    const data = Data
+    console.log("Data===>",data)
+    const ZoomMeetingNumber={
+    number:data.meeting_number,
+    pass:data.password
+    }
     navigate(`zoom-meeting/`, { state: { ZoomMeetingNumber } });
   }
+
+  const handleChangetabs = (event, newValue) => {
+    setValue(newValue);
+  };
+  
+  const handelClassView = (id)=>{
+    // navigate(`class_view/${id}`)
+    console.log("class view");
+  }
+
+  function formatApiDataForCalendar(apiData) {
+    const EventsData = apiData.map((clazz) => ({
+      title: clazz.title,
+      start: new Date(clazz.schedule),
+      end:"",
+      extendedProps: {
+        description: clazz.description,
+        teacher: clazz.teacher,
+        status: clazz.status,
+        students: clazz.students,
+        meeting_number: clazz.meeting_number
+      }
+    }));
+
+    setEvents(EventsData);
+    return EventsData;
+  }
+  const getAllClasses = async () => {
+    try {
+      const response = await axios.get(`${Base_url}api/classes`); // Update the API endpoint accordingly
+      setClasses(response.data.data);
+      const Data = response.data.data
+      if(Data){
+       
+          const formattedData = Data.map((item) => ({
+          "Title":item.title,
+         "Teacher":item.teacher.name,
+         "Date":item.schedule,
+        
+        "Recordings":"coming soon",
+        "View":<RemoveRedEyeIcon onClick={()=>handelClassView(item._id)}/>,
+       "Meeting":<Box style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+        {
+          item.meeting_number &&
+          <Button variant='contained' color='success'  onClick={()=>{handelZoomMeeting(item)}}>Join</Button>
+        }
+
+{
+          !item.meeting_number &&
+          <Button variant='contained' color='error'>not start yet</Button>
+        }
+        
+    </Box>
+     }));
+
+
+     formatApiDataForCalendar(Data)
+
+     setRows(formattedData);
+     setFilterRows(formattedData);
+   
+    
+    
+      }
+    } catch (error) {
+      console.error('Error fetching classes:', error.message);
+    }
+  };
+
+ 
+
+  useEffect(() => {
+    // Fetch all classes when the component mounts
+    getAllClasses();
+  }, [update]);
   return (
     <div>
-      <Card style={{borderRadius:"20px"}}>
-        <CardContent>
-        <FullCalendar
+      
+        <Box sx={{ width: '100%' }}>
+      <Box sx={{ display:"flex",justifyContent:"space-between",alignItems:"center" }}>
+      <ThemeProvider theme={orangeTheme}>
+        <Tabs value={value} onChange={handleChangetabs} aria-label="basic tabs example" textColor="primary"
+        indicatorColor="primary"
+       
+        >
+          <Tab label="table view" {...a11yProps(0)}  style={{backgroundColor:`${value === 0 ? "#EE731B" : "#fff"}`,marginRight:"10px",borderRadius:"10px",marginBottom:"10px"}}/>
+          <Tab label="calendar view" {...a11yProps(1)} style={{backgroundColor:`${value === 1 ? "#EE731B" : "#fff"}`,marginRight:"10px",borderRadius:"10px",marginBottom:"10px"}} />
+          
+        </Tabs>
+        </ThemeProvider>
+      </Box>
+      <CustomTabPanel value={value} index={0}>
+      <GenralTabel column={column} rows={rows} />
+      </CustomTabPanel>
+
+      <CustomTabPanel value={value} index={1}>
+      <FullCalendar
          defaultView="dayGridMonth"
          header={{
            left: "prev,next",
@@ -59,22 +219,20 @@ const LiveClasses = () => {
          }}
          themeSystem="Simplex"
          plugins={[dayGridPlugin]}
-         events={events}
+         events={Events}
          eventColor={"#" + Math.floor(Math.random() * 16777215).toString(16)}
          eventClick= {(info)=> {
-          alert('Event: ' + info.event.title);
-          // alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
-          // alert('View: ' + info.view.type);
-          handelZoomMeeting();
-          // change the border color just for fun
+          alert('Meeting: ' + info.event.title );
+       
+          // handelZoomMeeting();
+       
           info.el.style.borderColor = 'grey';
         }}
         
       />
-        </CardContent>
-      </Card>
- 
- <HostMeeting setZoomMeetingNumber={setZoomMeetingNumber}/>
+      </CustomTabPanel>
+
+    </Box>
  </div>
   )
 }
--- src/app/pages/Manage Client/User List/ZoomMeeetingRoom.jsx
+++ src/app/pages/Manage Client/User List/ZoomMeeetingRoom.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import ZoomMtgEmbedded from '@zoomus/websdk/embedded';
-import { Button } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import { useLocation, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { Base_url } from '../../../Config/BaseUrl';
@@ -13,16 +13,11 @@ export const ZoomMeeetingRoom = () => {
   const location = useLocation();
   const { ZoomMeetingNumber } = location.state; 
 
-  const [meetingData,setMeetingData] = useState({
-    meetingId:"",
-    meetingPassword:""
-  });
-
     const client = ZoomMtgEmbedded.createClient();
     var authEndpoint = Base_url
   var sdkKey = 'TsFvuPFLTeKf7_bNBWggPA'
-  var meetingNumber =meetingData.meetingId
-  var passWord = meetingData.meetingPassword
+  var meetingNumber =ZoomMeetingNumber.number
+  var passWord = ZoomMeetingNumber.pass
   var role = 0
   var userName = 'Akshay'
   var userEmail = 'akshay96102@gmail.com'
@@ -32,7 +27,7 @@ export const ZoomMeeetingRoom = () => {
   var userId="akshay96102@gmail.com"
   var SECRET="C7Dm4JuZ2QXoN0bM2OYTw5JxZvjPK1y9"
   
-  function getSignature(data) {
+  function getSignature() {
     const iat = Math.round(new Date().getTime() / 1000) - 30
     const exp = iat + 60 * 60 * 2
     const oHeader = { alg: 'HS256', typ: 'JWT' }
@@ -40,7 +35,7 @@ export const ZoomMeeetingRoom = () => {
     const oPayload = {
       sdkKey: sdkKey,
       appKey: sdkKey,
-      mn: data.meetingId,
+      mn: meetingNumber,
       role: role,
       iat: iat,
       exp: exp,
@@ -51,6 +46,11 @@ export const ZoomMeeetingRoom = () => {
     const sHeader = JSON.stringify(oHeader)
     const sPayload = JSON.stringify(oPayload)
     const sdkJWT = KJUR.jws.JWS.sign('HS256', sHeader, sPayload, SECRET);
+
+    const data = {
+      meetingId:meetingNumber,
+      meetingPassword:passWord
+    }
     setTimeout(()=>{
       startMeeting(sdkJWT,data)
     },1000)
@@ -116,26 +116,7 @@ export const ZoomMeeetingRoom = () => {
   }
 
   const handleJoinMeeting = async () => {
-    try {
-      const response = await axios.get(`${Base_url}api/meeting`);
-
-      if (response.status === 200) {
-        const meetingData = response.data;
-        const Data = meetingData.meeting
-        // Handle meeting data for the student
-        console.log('Meeting data:', Data);
-        const MData =Data[Data.length -1 ] 
-        setMeetingData(MData)
-        
-           getSignature(MData)
-       
-      } else {
-        // Handle error
-        console.error('Meeting not found');
-      }
-    } catch (error) {
-      console.error('Error joining meeting:', error);
-    }
+           getSignature()
   };
 
   const handelBack=()=>{
@@ -155,7 +136,15 @@ export const ZoomMeeetingRoom = () => {
 <div id="meetingSDKElement" >
           {/* Zoom Meeting SDK Component View Rendered Here */}
         </div>
-   {/* <Button onClick={getSignature}>Join Meeting</Button> */}
+
+{
+  !ZoomMeetingNumber && 
+  <Box style={{textAlign:"center",border:"1px solid red"}}>
+    <h2 style={{fontSize:"30px"}}>Meeting credentials not matched !!</h2>
+    <h2 style={{fontSize:"16px"}}>Go back refresh and try again</h2>
+    <Button sx={{marginTop:"30px"}} variant='contained'  onClick={handelBack} >Go Back</Button>
+  </Box>
+}
     </div>
 
     
